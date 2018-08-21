b1acafebb5678c0f4e21f2a2e9a15aba72d22991
ASUS <ASUS@LAPTOP-B25R1O34>
2018-08-21 21:20:02 +0700

    Initial Commit

---
+++ src/jobsheet9indira/Latihan3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet9indira;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Latihan3 {
+  public static int a (int x, int y, int z)
+  {
+      return x + y + z;
+  }
+      public static void main(String [] args){
+      int a;
+      a = a (1,1,1);
+      
+      }
+}
+
+  
